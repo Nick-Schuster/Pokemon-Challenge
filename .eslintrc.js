f9bbc432ddb0d7e54d94ada664044b540e9c6631
Nico Schiffer <nsi@novatec-gmbh.de>
2025-04-15 10:28:42 +0200

    implementation of Git Actions -> Buid Pipeline
    implementation test (Jest) + pipeline update
    
    implementation test (Jest) + pipeline updated
    
    implementation test (Jest) + pipeline updated
    
    implementation test (Jest) + pipeline updated
    
    implementation test (Jest) + pipeline updated
    
    implementation test (Jest) + pipeline updated
    
    implementation test (Jest) + pipeline updated
    
    implementation test (Jest) + pipeline updated
    
    implementation test (Jest) + pipeline updated
    
    Fix for Pipeline and build errors

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM node:18-alpine
+FROM node:20-alpine
 
 WORKDIR /app
 
 COPY package*.json ./
 RUN npm install
 
-COPY public/ public/
-COPY src/ src/
+COPY public/ ./
+COPY src/ ./
 COPY tsconfig.json ./
+COPY jest-setup.js ./
+COPY jest.config.js ./
+COPY .babelrc ./
+
+
 
 EXPOSE 3000
 CMD ["npm", "start"]
